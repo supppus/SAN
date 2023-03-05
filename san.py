9ab36eb989c50aea2592622902f860dde3758c4c
Victor <121402298+supppus@users.noreply.github.com>
2023-03-05 00:51:08 -0400

    Update san.py

--- san.py
+++ san.py
@@ -1,30 +1,36 @@
 import openai
 
-#San context for prompt
-san_context = "This is a discussion between a human and an AI, the human says something, and the AI must analyze the text sent by the human and respond with the subject that is in the text."
-openai.api_key = "YOUR_API_TOKEN_OPENAI"
+class San:
+    def __init__(self, text):
+        self.request = text
 
+        # Config
+        self.sanContext = "This is a discussion between a human and an AI, the human says something, and the AI must analyze the text sent by the human and respond with the subject that is in the text."
+        openai.api_key = "YOUR_API_TOKEN_OPENAI"
 
-def san(a):
-    with open('san.data') as f:
-        lines = f.readlines()
-        response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt="{}\n\n{}Human:{} \nAI:".format(san_context, lines, a),
-        temperature=0.9,
-        max_tokens=1000,
-        top_p=1,
-        frequency_penalty=0.0,
-        presence_penalty=0.6,
-        stop=[" Human:", " AI:"]
-        )
-        print("SAN : {}".format(response['choices'][0]['text']))
-        resp= response['choices'][0]['text'].lower()
+    def __Verify(self):
+        with open('sa.data') as Archive:
+            self.Lines = Archive.readlines()
+            self.response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt="{}\n\n{}Human:{} \nAI:".format(self.sanContext, self.Lines, self.request),
+            temperature=0.9,
+            max_tokens=1000,
+            top_p=1,
+            frequency_penalty=0.0,
+            presence_penalty=0.6,
+            stop=[" Human:", " AI:"]
+            )
+            self.SAN = self.response['choices'][0]['text'].lower()
 
-        if resp.find("crime") != -1 or resp.find("offense") != -1 or resp.find("malware") != -1 or resp.find("racism") != -1 or resp.find("porn") != -1  or resp.find("chemical") != -1 or resp.find("personality") != -1:
-            return "SAN: Not accepted."
-        else:
-            return "SAN: Acceptable."
-
+            if self.SAN.find("crime") != -1 or self.SAN.find("offense") != -1 or self.SAN.find("malware") !=-1 or self.SAN.find("racism") != -1 or self.SAN.find("porn") != -1  or self.SAN.find("chemical") != -1 or self.SAN.find("personality") != -1:
+                return "SAN: Not accepted."
+            else:
+                return "SAN: Acceptable."
+            
+    def Work(self):
+        return self.__Verify()
+    
 while True:
-    print(san(input("You : ")))
+    SAN = San(input("Your text-> "))
+    SAN.Work()
